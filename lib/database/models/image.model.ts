686949f18e03e75a21c6283264877e655dc74aac
misterbr6wn <nutchaphon121@gmail.com>
2024-02-17 23:44:39 +0700

    final?

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,32 +4,32 @@ export interface IImage extends Document {
     title: string;
     transformationType: string;
     publicId: string;
-    secureUrl: string;
+    secureURL: string; 
     width?: number;
     height?: number;
-    config?: object;
-    transformationUrl?: string;
+    config?: object; 
+    transformationUrl?: string; 
     aspectRatio?: string;
     color?: string;
     prompt?: string;
     author: {
-        _id: string,
-        firstName: string,
-        lastname: string
-    };
+      _id: string;
+      firstName: string;
+      lastName: string;
+    }
     createdAt?: Date;
     updatedAt?: Date;
-}
-
+  }
+  
 const ImageSchema = new Schema({
     title: { type: String, require: true },
     transformationType: { type: String, require: true },
     publicId: { type: String, require: true },
-    secureUrl: { type: URL, require: true },
+    secureURL: { type: String, require: true },
     width: { type: Number },
     height: { type: Number },
     config: { type: Object },
-    transformationUrl: { type: URL },
+    transformationUrl: { type: String },
     aspectRatio: { type: String },
     color: { type: String },
     prompt: { type: String },
