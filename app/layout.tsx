4560d54079db029f0519ce5a5b231f8b29ff0f11
misterbr6wn <nutchaphon121@gmail.com>
2024-02-18 00:01:26 +0700

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
-  title: "Leaf-Imaginify",
+  title: "Imaginify",
   description: "AI-powered image generator",
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: ''
-        }
-      ]
-    }
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: ''
+      }
+    ]
+  }
+};
+
+export default nextConfig;
